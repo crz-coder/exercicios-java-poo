13170b88c05878c3aeedbc082efe9a75d75abeb2
crz-code <cristoferom27@gmail.com>
2026-06-25 18:08:34 -0400

    adicionando o desconto de uso do cheque especial e seu pagamento automatico

--- src/Conta.java
+++ src/Conta.java
@@ -2,10 +2,12 @@ public class Conta {
 
     private double saldo;
     private double chequeEsp;
+    private double limiteEsp;
 
     public Conta(double saldo, double chequeEsp) {
         this.saldo = saldo;
         this.chequeEsp = chequeEsp;
+        this.limiteEsp = chequeEsp;
     }
     public double getSaldo() {
         return saldo;
@@ -14,7 +16,13 @@ public class Conta {
         return chequeEsp;
     }
     public void depositar(double valorDepositado){
-        this.saldo += valorDepositado;
+        if (chequeEsp < limiteEsp) {
+            double resto = limiteEsp - chequeEsp;
+            double juros = resto * 0.2;
+            valorDepositado -= resto + juros;
+            this.chequeEsp += resto;
+            this.saldo += valorDepositado;
+        } else this.saldo += valorDepositado;
     }
     public void sacar(double valorSacado){
         if ((valorSacado > saldo) && (valorSacado <= saldo + chequeEsp)){
